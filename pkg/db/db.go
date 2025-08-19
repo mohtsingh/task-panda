4e53d2ac627672f7cd5cffb3c1ec480652fc68ba
Ayush Mahariya <ayushmahariya123@gmail.com>
2025-08-20 00:36:46 +0530

    Update db.go

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -6,30 +6,26 @@ import (
 	"log"
 	"os"
 
-	// This is for only Ayush's setup
-	"github.com/joho/godotenv"
-	// .................
-
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No .env file found or failed to load")
-	}
+	var err error
 	connStr := os.Getenv("DATABASE_URL")
 	if connStr == "" {
 		log.Fatal("DATABASE_URL environment variable not set")
 	}
+
 	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if err = DB.Ping(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Connected to the database!")
+
+	fmt.Println("✅ Connected to the database!")
 }
