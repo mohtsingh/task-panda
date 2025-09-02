7fe95cc218ebb04b3d665a6cb8faceb42cdd33f3
Ayush Mahariya <ayushmahariya123@gmail.com>
2025-09-02 12:04:11 +0530

    Update types.go of offers

--- pkg/offers/types.go
+++ pkg/offers/types.go
@@ -11,3 +11,9 @@ type Offer struct {
 	UpdatedAt    string  `json:"updated_at"`
 	ProviderName string  `json:"provider_name,omitempty"`
 }
+
+// UpdateOfferRequest represents the JSON request body for updating an offer
+type UpdateOfferRequest struct {
+	OfferedPrice *float64 `json:"offered_price,omitempty"`
+	Message      *string  `json:"message,omitempty"`
+}
